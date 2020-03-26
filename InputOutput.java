453233ee520db2f707d04710f61ea46bd9476b2b
Rofiah123 <62419412+Rofiah123@users.noreply.github.com>
2020-03-26 13:36:27 +0700

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -4,7 +4,7 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
-  System.out.print("Rofi'ah: ");
+  System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
  }
